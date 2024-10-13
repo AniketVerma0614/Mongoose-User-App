4d25ddbd60ac5b042d00117f519fe3b5b8a7405e
AniketVerma0614 <aniketverma0614@gmail.com>
2024-10-13 09:34:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,12 +21,33 @@ const userSchema = new mongoose.Schema({
 //Mongoose MODEL ...
 const User = mongoose.model("User",userSchema);
 
-//Will use the find 
-User.findById("670ab84ea3948b4c20a06138")
-    .then((res)=>{
-        console.log(res);
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
+// //Will use the find 
+// User.findById("670ab84ea3948b4c20a06138")
+//     .then((res)=>{
+//         console.log(res);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     });
 
+// User.findOneAndUpdate({name: "Bruce"},{age: 100}).then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+User.findByIdAndDelete('670ab807c2481ac52a00f5b6').then(res =>{
+    console.log(res);
+});
+
+/*
+PS C:\Users\HP\OneDrive\Desktop\DELTA4\MONGO> node index.js
+connection successful !!!
+{
+  _id: new ObjectId('670ab807c2481ac52a00f5b6'),
+  name: 'Eve',
+  email: 'eve@yahoo.com',
+  age: 555,
+  __v: 0
+}
+*/
