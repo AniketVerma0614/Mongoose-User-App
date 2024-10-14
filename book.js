61c8997d7e2ed35017dac1736ffc969a7b56ca16
AniketVerma0614 <aniketverma0614@gmail.com>
2024-10-14 08:27:37 +0530

    Add Book schema with validation and findByIdAndUpdate functionality
    - Created a Book schema with fields: title, author, price, discount, and category.
    - Added validation rules to ensure data integrity (e.g., maxLength, required, min values).
    - Implemented the `findByIdAndUpdate` method to update book details with validation.
    - Included error handling for validation errors to display meaningful error messages.

--- book.js
+++ book.js
@@ -11,3 +11,35 @@ console.log("connection successful !!!");
 async function main() {
 await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
 }
+
+//Defining the Book Schema !!!
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required : true,
+        maxLength: 20,
+    },
+    author: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        min: 1,
+    },
+    discount: {
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: String,
+        enum: ["fiction","non-fiction"]
+    }
+});
+
+const Book = mongoose.model("Book",bookSchema);
+
+Book.findByIdAndUpdate("670b614585a36643ec17ba54",{price: -500},{runValidators: true}).then((res)=>{
+    console.log(res);
+}).catch((err)=>{
+    console.log(err.errors.price.properties.message);
+});
